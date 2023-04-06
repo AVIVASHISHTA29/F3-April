a87402050ce942c9f07d6e3464a842acdb59cb60
avivashishta29 <avivashishta@gmail.com>
2023-04-06 21:48:02 +0530

    oops intro

---
+++ oops.js
@@ -0,0 +1,51 @@
+console.log("OOPS");
+
+// Why OOPS?
+
+// var user_1_name = "Avi Vashishta";
+// var user_1_profession = "Instructor";
+// var user_1_gender = "Male";
+// var user_1_phone = "9999999999";
+// var user_1_email = "avivashishta@gmail.com";
+// var user_1_history = "History";
+
+// var user_2_name = "Sunil";
+// var user_2_profession = "Student";
+// var user_2_gender = "Male";
+// var user_2_phone = "9999999999";
+// var user_2_email = "sunil@gmail.com";
+// var user_2_history = "History";
+
+// var user_3_name = "Sunil 2";
+// var user_3_profession = "Student";
+// var user_3_gender = "Male";
+// var user_3_phone = "9999999999";
+// var user_3_email = "sunil@gmail.com";
+// var user_3_history = "History";
+
+var user1 = {
+  name: "Avi",
+  email: "avi@gmail.com",
+  phone: "99999999",
+  histiry: "histiry",
+  gender: "male ",
+};
+
+var user2 = {
+  name: "Avi",
+  email: "avi@gmail.com",
+  phone: "99999999",
+  histiry: "histiry",
+  gender: "male ",
+};
+
+class Dog
+{
+    bark()
+    drink()
+    eat()
+    // colour
+}
+
+var myDog = new Dog();
+myDog.bark()
