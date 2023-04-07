b4dd410d295616b8eeca1a0e74e35a6442133fae
avivashishta29 <avivashishta@gmail.com>
2023-04-07 21:30:34 +0530

    oops beginning

--- oops.js
+++ oops.js
@@ -1,4 +1,4 @@
-console.log("OOPS");
+// console.log("OOPS");
 
 // Why OOPS?
 
@@ -23,29 +23,105 @@ console.log("OOPS");
 // var user_3_email = "sunil@gmail.com";
 // var user_3_history = "History";
 
+// JSON - JavaScript Object Notation
+
 var user1 = {
   name: "Avi",
   email: "avi@gmail.com",
   phone: "99999999",
-  histiry: "histiry",
+  history: "histiry",
   gender: "male ",
 };
 
-var user2 = {
-  name: "Avi",
-  email: "avi@gmail.com",
-  phone: "99999999",
-  histiry: "histiry",
-  gender: "male ",
-};
+// console.log("User1>>>", user1);
 
-class Dog
-{
-    bark()
-    drink()
-    eat()
-    // colour
+// Factory Function
+function createUser(nameVal, emailVal, phoneVal, historyVal, genderVal) {
+  return {
+    name: nameVal,
+    email: emailVal,
+    phone: phoneVal,
+    history: historyVal,
+    gender: genderVal,
+  };
 }
 
-var myDog = new Dog();
-myDog.bark()
+var user2 = createUser(
+  "Ajay",
+  "ajay@gmail.com",
+  "98888888",
+  "historyt",
+  "male"
+);
+
+// console.log("User2>>>", user2);
+
+// There are multiple ways to create what we call as an object in JS.
+// What is an object?
+
+// An Object is nothing but it is an instance of a class
+// What is a class? A class is a blue print.
+
+// Every user will have a name, email,phone, history, gender --> Blueprint that I have created,
+
+class User {
+  constructor(nameVal, emailVal, phoneVal, historyVal, genderVal) {
+    this.name = nameVal;
+    this.email = emailVal;
+    this.phone = phoneVal;
+    this.history = historyVal;
+    this.gender = genderVal;
+  }
+
+  getUserName() {
+    return "The User's Name is " + this.name;
+  }
+
+  speak() {
+    return "The User speaks their native language";
+  }
+}
+
+// instead of saying key,value pair -> I will call name,email,phoone,etc. is a property
+
+// Constructor is a function which gets called when you first initialise an object. Whenever you create an object, its constructor function gets called,
+// How do you create a new object? By using the keyword new.
+const user3 = new User(
+  "Ajay 2",
+  "ajay2@gmail.com",
+  "22222222",
+  "history2",
+  "male 2"
+);
+
+console.log(user3.getUserName());
+console.log(user3.name);
+
+const user4 = new User(
+  "Sunil",
+  "sunil@gmail.com",
+  "8127261219",
+  "history uqyg",
+  "male"
+);
+
+// console.log(user4);
+
+// function createUser(nameVal, emailVal, phoneVal, historyVal, genderVal) {
+//   return {
+//     name: nameVal,
+//     email: emailVal,
+//     phone: phoneVal,
+//     history: historyVal,
+//     gender: genderVal,
+//   };
+// }
+
+// console.log(user1.name, user2.name, user3.name);
+
+// function helloWorld() {
+//   return "Hello world";
+// }
+
+// console.log(helloWorld);
+// console.log(helloWorld());
