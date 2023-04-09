df9c5da3c28fba7280a4cc43cb52c89539fd0565
avivashishta29 <avivashishta@gmail.com>
2023-04-09 14:59:26 +0530

    async await

--- async.js
+++ async.js
@@ -4,16 +4,49 @@
 // JS can also be `Asynchronous` - is just the absense of synchronous. It colur be running parallely, it could be taking some time to run, it could not be following the flow
 // What is the colour light, darkness?
 
-console.log("1", new Date());
+// console.log("hi0", new Date());
 
-setTimeout(() => {
-  console.log("2", new Date());
-}, 1000);
+// setTimeout(() => {
+//   console.log("hi1", new Date());
+// }, 1000);
 
-setTimeout(() => {
-  console.log("3", new Date());
-}, 2000);
+// setTimeout(() => {
+//   console.log("hi2", new Date());
+// }, 2000);
 
-setTimeout(() => {
-  console.log("4", new Date());
-}, 3000);
+// setTimeout(() => {
+//   console.log("hi3", new Date());
+// }, 3000);
+
+function myPromise() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      reject({ data: "hello", status: "ok" });
+    }, 2000);
+  });
+}
+// Problem statment is -> Create a fnc to handle a promise
+
+// async function myAysncFnc() {
+//   console.log("my function", new Date());
+//   const myPr = await myPromise().catch((err) => console.log("ERROR", err));
+//   // WAIT TILL PROMISE GETS FULFILLED THEN CONSOLE IT
+//   if (myPr) {
+//     console.log("MY PR", myPr, new Date());
+//   }
+// }
+
+async function myAsyncFnc() {
+  console.log("my function", new Date());
+  try {
+    const myPr = await myPromise();
+    // WAIT TILL PROMISE GETS FULFILLED THEN CONSOLE IT
+    if (myPr) {
+      console.log("MY PR", myPr, new Date());
+    }
+  } catch (e) {
+    console.log("ERROR 2", e);
+  }
+}
+
+myAsyncFnc();
