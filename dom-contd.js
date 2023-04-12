f0a2eab2e476059af37c0d16b1dca33ec192be11
avivashishta29 <avivashishta@gmail.com>
2023-04-12 21:53:46 +0530

    dom contd

---
+++ dom-contd.js
@@ -0,0 +1,50 @@
+console.log("Dom Continued...");
+
+// var myDiv = document.getElementById("myDiv");
+// console.log(myDiv);
+
+// const imgSrc =
+//   "https://t1.gstatic.com/licensed-image?q=tbn:ANd9GcRPMKnq00NF_T7RusUNeLrSazRZM0S5O8_AOcw2iBTmYTxd3Q7uXf0sW41odpAKqSblKDMUMHGb8nZRo9g";
+
+// // myDiv.innerText = "<h1>Hi How Are You</h1>";
+
+// function addDiv() {
+//   console.log("Mouse Is Clicked");
+//   myDiv.innerHTML = `<h1>Hi How Are You</h1><div><img src='${imgSrc}' height='100' /></div>`;
+//   console.log(myDiv);
+// }
+
+// // var myBtn = document.getElementById("btn");
+// var myBtn = document.querySelector("#btn");
+// console.log(myBtn);
+
+// // Same thing, but in a different way
+// myBtn.addEventListener("click", addDiv);
+
+// myBtn.addEventListener("mouseover", () => {
+//   console.log("Mouse Is Over The Button");
+// });
+
+// myBtn.addEventListener("mousedown", () => {
+//   console.log("Mouse Down");
+// });
+
+// myBtn.addEventListener("mouseup", () => {
+//   console.log("Mouse Up");
+// });
+
+function login() {
+  var myEmailVal = document.getElementById("my-email-input").value;
+  var myPassVal = document.getElementById("my-pass-input").value;
+  console.log(myEmailVal, myPassVal);
+}
+
+var loginBtn = document.getElementById("login-btn");
+loginBtn.addEventListener("click", login);
+
+//  What if I want to console.log value that the user is inputting at that instant
+
+var myEmailInput = document.getElementById("my-email-input");
+myEmailInput.addEventListener("change", () => {
+  console.log(myEmailInput.value);
+});
