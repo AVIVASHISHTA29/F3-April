be7d122f9537b83c0317266501cf816104228b87
avivashishta29 <avivashishta@gmail.com>
2023-04-12 22:58:00 +0530

    modal

---
+++ modal.js
@@ -0,0 +1,9 @@
+function openModal() {
+  var myContainer = document.getElementById("modal-container-id");
+  myContainer.style.display = "flex";
+}
+
+function closeModal() {
+  var myContainer = document.getElementById("modal-container-id");
+  myContainer.style.display = "none";
+}
