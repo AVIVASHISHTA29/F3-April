53b890d80a5963cd318061e7296776da05957aa6
avivashishta29 <avivashishta@gmail.com>
2023-04-13 21:20:52 +0530

    dom done

--- creatingElements.js
+++ creatingElements.js
@@ -1,1 +1,48 @@
 console.log("Creating Elements");
+
+const imgSrc =
+  "https://cdn.pixabay.com/photo/2016/02/19/15/46/labrador-retriever-1210559__480.jpg";
+// document.getElementsByTagName("body")[0].appendChild(heading);
+
+// let arr = [1, 2, 3];
+
+// append `4` in this array
+
+// A. [4,1,2,3]
+// B. [1,4,2,3]
+// C. [1,2,4,3]
+// D. [1,2,3,4]
+
+var count = 0;
+
+function appendElements() {
+  var listItem = document.createElement("li");
+  listItem.innerText = count;
+
+  listItem.setAttribute("id", `my-id-${count}`);
+  listItem.setAttribute("title", `my-title-${count}`);
+  listItem.setAttribute("class", `list-item`);
+
+  // listItem.removeAttribute("class");
+  // listItem.classList.add("123");
+
+  // listItem.classList.remove("list-item");
+
+  // If class is there remove it, else add it
+  listItem.classList.toggle("123");
+
+  document.getElementById("list").appendChild(listItem);
+
+  // document.getElementById("list").innerHTML += `
+  //     <li class="list-item">${count}</li>
+  //   `;
+
+  // var row = document.createElement("tr");
+  // row.innerHTML = `<td>${count}</td>`;
+  // document.getElementById("table").appendChild(row);
+
+  count++;
+}
+
+// var newElement = document.createElement("div");
+// console.log(newElement);
