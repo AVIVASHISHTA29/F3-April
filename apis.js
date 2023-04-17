cf215ad616903279ea25d124ec9861c50a484a30
avivashishta29 <avivashishta@gmail.com>
2023-04-17 22:15:32 +0530

    APIs Intro

---
+++ apis.js
@@ -0,0 +1,5 @@
+// You as a client send a request package.
+// Server sends a response package.
+
+// 4 Basic "methods" (intentions) with which you hit a URL.
+//  GET, POST (Create), UPDATE, DELETE
--- fetchingData.js
+++ fetchingData.js
@@ -8,17 +8,74 @@ console.log("fetching data");
 
 // inbuilt function that returns a promise which resolves into data
 
-// fetch("https://dummyjson.com/products/1")
+// fetch(url, { method: "GET" })
 //   .then((response) => {
+//     console.log("Resonse object", response);
 //     return response.json();
 //   })
 //   .then((data) => {
 //     console.log("DATA", data);
-//     // console.log(data.quote);
-//     // console.log(data.author);
+//     let quotesArr = data.quotes;
+//     console.log("QUOTES", quotesArr);
+
+//     quotesArr.forEach((data) => {
+//       document.getElementById("my-div").innerHTML += `
+//         <h2>${data.id} - ${data.author}</h2>
+//         <p>${data.quote}</p>
+//     `;
+//     });
 //   })
 //   .catch((err) => {
 //     console.log("ERROR", err);
 //   });
 
-// document.body.addChild
+// fetch("url")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log("DATA", data);
+//   });
+
+// let url = "https://dummyjson.com/products/add";
+// POST is used when you want to create.
+// example - Post something on instagram
+
+// let options = {
+//   method: "POST",
+//   headers: { "Content-Type": "application/json" },
+//   body: JSON.stringify({
+//     title: "BMW Pencil 2",
+//     description: "Pencil for people who drive BMWs",
+//     price: 1000000,
+//     discountPercentage: 0.0,
+//     rating: 5.0,
+//     stock: 10,
+//     brand: "BMW 2",
+//     category: "pencils",
+//   }),
+// };
+
+// fetch(url, options)
+//   .then((res) => res.json())
+//   .then((data) => console.log(data));
+
+// update
+let url = "https://dummyjson.com/products/1";
+
+let options = {
+  method: "PUT" /* or PATCH */,
+  headers: { "Content-Type": "application/json" },
+  body: JSON.stringify({
+    title: "Avi",
+    price: 10000,
+  }),
+};
+
+fetch(url, options)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
+
+// var obj = '{"name":"avi","id":1}';
+// console.log(obj);
+// console.log(JSON.parse(obj));
